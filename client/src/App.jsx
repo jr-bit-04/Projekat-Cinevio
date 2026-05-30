6ecae3b1d4a65df539ffd40e44ca51becaf37b6c
jr04 <jovanarakonjac2004@gmail.com>
2026-05-30 22:36:52 +0200

    fix route definitions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,97 +30,26 @@ function App() {
         {/* PUBLIC ROUTES */}
 
         <Route path="/" element={<Home />} />
-
         <Route path="/movies" element={<Movies />} />
-
         <Route path="/series" element={<Series />} />
-
         <Route path="/details/:id" element={<Details />} />
-
-        <Route
-          path="/series-details/:id"
-          element={<SeriesDetails />}
-        />
-
+        <Route path="/series-details/:id" element={<SeriesDetails />}/>
         <Route path="/login" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
 
         {/* PROTECTED USER ROUTES */}
 
-        <Route
-          path="/watchlist"
-          element={
-            <ProtectedRoute>
-              <Watchlist />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/top-movies"
-          element={
-            <ProtectedRoute>
-              <TopMovies />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/top-series"
-          element={
-            <ProtectedRoute>
-              <TopSeries />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/recommendations"
-          element={
-            <ProtectedRoute>
-              <Recommendations />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/request-content"
-          element={
-            <ProtectedRoute>
-              <ContentRequest />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/watchlist" element={<ProtectedRoute><Watchlist /></ProtectedRoute>} />
+        <Route path="/top-movies" element={<ProtectedRoute><TopMovies /></ProtectedRoute>} />
+        <Route path="/top-series" element={<ProtectedRoute><TopSeries /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/recommendations" element={<ProtectedRoute><Recommendations /></ProtectedRoute>} />
+        <Route path="/request-content" element={<ProtectedRoute><ContentRequest /></ProtectedRoute>} />
 
         {/* ADMIN ROUTE */}
 
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute adminOnly={true}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/admin/users"
-          element={
-            <ProtectedRoute adminOnly={true}>
-              <AdminUsers />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/admin" element={<ProtectedRoute adminOnly={true}><AdminDashboard /></ProtectedRoute>} />
+        <Route path="/admin/users" element={<ProtectedRoute adminOnly={true}><AdminUsers /></ProtectedRoute>} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
